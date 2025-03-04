e8f025e0d7468a7b73297152ae5fb0dd6541c1d5
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-03-04 16:14:37 -0600

    Hero component implemented

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	interface Props {
+		imgURL?: string
+		overlay?: boolean
+		id: string
+		heading?: Snippet
+		body?: Snippet
+	}
+
+	const { imgURL, overlay, id, heading, body }: Props = $props()
+</script>
+
+<div
+	class="grid min-h-screen min-w-screen place-items-center bg-cover bg-center"
+	style:background-image="url({imgURL})"
+>
+	{#if overlay}
+		<div class="bg-neutral col-start-1 row-start-1 h-screen w-screen opacity-50"></div>
+	{/if}
+
+	<div
+		class={[
+			'z-0 col-start-1 row-start-1 my-24 flex max-w-[60ch] flex-col items-center justify-center',
+			overlay &&
+				'prose-headings:text-primary prose-a:text-primary prose-a:bg-[url(/src/lib/assets/1x4-00adb5ff.png)] text-center',
+		]}
+		style="padding: 0 clamp(1rem, 5%, 2rem);"
+	>
+		<div>
+			{@render heading?.()}
+		</div>
+		<div>
+			{@render body?.()}
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,62 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Hero from '$lib/components/Hero.svelte'
+	import imgURL from '$lib/assets/ivan-bandura-XsAz9Mq61XY-unsplash.jpg'
+</script>
+
+<Hero {imgURL} overlay id="welcome">
+	{#snippet heading()}
+		<h1 class="animate-fly-up-slow">
+			Filling Southern MN with healthy churches
+			<a href="#imaginary-1" class="font-bold">for the next 100 years.</a>
+		</h1>
+	{/snippet}
+</Hero>
+
+<Hero id="imaginary-1">
+	{#snippet heading()}
+		<h1>Jesus is decidedly unstrategic.</h1>
+	{/snippet}
+	{#snippet body()}
+		<p>
+			He came to the sound of angels singing and then spent the next thirty years learning
+			carpentry. Even during his public ministry, he spent most of his time going at a slow pace in
+			a small place. This is unstrategic not because there was no plan, but because the plan doesn’t
+			make sense! Yet the madness of this plan is exactly how God chose to display the upside-down
+			nature of his Kingdom. Jesus won by dying. Jesus chose two women to witness the resurrection.
+			Jesus built his church on the preaching of one of his betrayers. Jesus' plan is beautiful—but
+			mad. <a href="#imaginary-2">Believing it takes imagination.</a>
+		</p>
+	{/snippet}
+</Hero>
+
+<Hero id="imaginary-2">
+	{#snippet heading()}
+		<h1>Many view rural ministry as unstrategic.</h1>
+	{/snippet}
+	{#snippet body()}
+		<p>
+			Yet we believe rural ministry presents a unique opportunity to display the very upside-down
+			gospel we proclaim. Going at a slow pace in a small place as we win by dying embodies the
+			gospel logic of John 12:24:
+			<em>
+				“Truly, truly, I say to you, unless a grain of wheat falls into the earth and dies, it
+				remains alone; but if it dies, it bears much fruit."
+			</em>
+			The slowness and smallness of this kind of ministry requires a certain kind of
+			<strong>gospel imaginary</strong>
+			from those who pastor here. It’s the kind of imaginary Wendell Berry calls for in his poem
+			<em>Manifesto: The Mad Farmer Liberation Front:</em>
+		</p>
+		<pre>So, friends, every day do something
+that won’t compute....
+Invest in the millennium. Plant sequoias.
+Say that your main crop is the forest
+that you did not plant,
+that you will not live to harvest.</pre>
+		<p>
+			We’ve caught this madness. And out of this gospel madness flows <a href="#vision"
+				>our 100 year vision.</a
+			>
+		</p>
+	{/snippet}
+</Hero>
